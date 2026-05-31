ac073992661807a779f103c673691c57a63de755
VIPUL KUMAR PATEL <158318719+Vipul99999@users.noreply.github.com>
2026-05-31 09:27:31 +0530

    Create UpperBoundSearch.js

---
+++ Divide-and-Conquer Searching/Binary Search/Upper Bound Search/UpperBoundSearch.js
@@ -0,0 +1,39 @@
+function upperBoundSearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    let answer = arr.length;
+
+    while (left <= right) {
+        const mid =
+            left + Math.floor((right - left) / 2);
+
+        if (arr[mid] > target) {
+            answer = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return answer;
+}
+
+const arr = [1, 3, 3, 3, 5, 7, 9];
+const target = 3;
+
+const index = upperBoundSearch(
+    arr,
+    target
+);
+
+if (index < arr.length) {
+    console.log(
+        `Upper Bound of ${target} ` +
+        `is at index ${index}`
+    );
+} else {
+    console.log(
+        `Upper Bound position is at ` +
+        `end of array (${index})`
+    );
+}
