191db9cdb2ba183b3de0ead0ad89f477905609bf
VIPUL KUMAR PATEL <158318719+Vipul99999@users.noreply.github.com>
2026-05-31 09:32:44 +0530

    Create FirstOccuranceSearch.py

---
+++ Divide-and-Conquer Searching/Binary Search/First Occurrence Search/FirstOccuranceSearch.py
@@ -0,0 +1,41 @@
+def first_occurrence_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    answer = -1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if arr[mid] == target:
+            answer = mid
+            right = mid - 1
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return answer
+
+
+def main():
+    arr = [1, 3, 3, 3, 5, 7, 9]
+    target = 3
+
+    index = first_occurrence_search(
+        arr,
+        target
+    )
+
+    if index != -1:
+        print(
+            f"First occurrence of {target} "
+            f"found at index {index}"
+        )
+    else:
+        print(
+            f"{target} not found in array"
+        )
+
+
+if __name__ == "__main__":
+    main()
