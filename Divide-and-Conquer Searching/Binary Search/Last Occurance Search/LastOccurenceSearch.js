71328e60e59647caf991a06aa435922616af51cc
VIPUL KUMAR PATEL <158318719+Vipul99999@users.noreply.github.com>
2026-05-31 09:38:48 +0530

    Create LastOccurenceSearch.js

---
+++ Divide-and-Conquer Searching/Binary Search/Last Occurance Search/LastOccurenceSearch.js
@@ -0,0 +1,51 @@
+function lastOccurrenceSearch(
+    arr,
+    target
+) {
+    let left = 0;
+    let right = arr.length - 1;
+    let answer = -1;
+
+    while (left <= right) {
+        const mid =
+            left + Math.floor(
+                (right - left) / 2
+            );
+
+        if (arr[mid] === target) {
+            answer = mid;
+            left = mid + 1;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return answer;
+}
+
+const arr = [
+    1, 3, 3, 3, 5, 7, 9
+];
+
+const target = 3;
+
+const index =
+    lastOccurrenceSearch(
+        arr,
+        target
+    );
+
+if (index !== -1) {
+    console.log(
+        `Last occurrence of ${target} ` +
+        `found at index ${index}`
+    );
+} else {
+    console.log(
+        `${target} not found in array`
+    );
+}
