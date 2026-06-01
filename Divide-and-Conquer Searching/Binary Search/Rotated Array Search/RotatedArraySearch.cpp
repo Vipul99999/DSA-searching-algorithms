c84c503d57bf35666448690e9227600998b8fd35
VIPUL KUMAR PATEL <158318719+Vipul99999@users.noreply.github.com>
2026-06-01 10:58:47 +0530

    Implement Rotated Array Search algorithm

---
+++ Divide-and-Conquer Searching/Binary Search/Rotated Array Search/RotatedArraySearch.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+class RotatedArraySearch
+{
+public:
+
+    static int search(
+        const std::vector<int>& nums,
+        int target)
+    {
+        int left = 0;
+        int right =
+            static_cast<int>(
+                nums.size()
+            ) - 1;
+
+        while (left <= right)
+        {
+            int mid =
+                left +
+                (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid])
+            {
+                if (target >= nums[left] &&
+                    target < nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            else
+            {
+                if (target > nums[mid] &&
+                    target <= nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    std::vector<int> nums =
+    {
+        4, 5, 6, 7, 0, 1, 2
+    };
+
+    int target = 0;
+
+    std::cout
+        << "Index: "
+        << RotatedArraySearch::search(
+               nums,
+               target
+           )
+        << '\n';
+
+    return 0;
+}
