8aebbedaf57792276808ce9a5f4718f70db7e41b
VIPUL KUMAR PATEL <158318719+Vipul99999@users.noreply.github.com>
2026-06-05 09:09:12 +0530

    Implement adaptive interpolation search algorithm

---
+++ Distribution-Based Searching/Adaptive Interpolation Search/AdaptiveInterpolationSearch.py
@@ -0,0 +1,83 @@
+def adaptive_interpolation_search(arr, target):
+
+    low = 0
+    high = len(arr) - 1
+
+    while (
+        low <= high
+        and target >= arr[low]
+        and target <= arr[high]
+    ):
+
+        if arr[low] == arr[high]:
+            return (
+                low
+                if arr[low] == target
+                else -1
+            )
+
+        pos = low + int(
+            (
+                (target - arr[low])
+                * (high - low)
+            )
+            /
+            (
+                arr[high]
+                - arr[low]
+            )
+        )
+
+        if pos < low or pos > high:
+            pos = (low + high) // 2
+
+        if arr[pos] == target:
+            return pos
+
+        if arr[pos] < target:
+            low = pos + 1
+        else:
+            high = pos - 1
+
+        if high - low < 5:
+
+            mid = (
+                low + high
+            ) // 2
+
+            if arr[mid] == target:
+                return mid
+
+            if arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+    return -1
+
+
+arr = [
+    10,
+    20,
+    25,
+    35,
+    50,
+    70,
+    100,
+    150
+]
+
+target = 70
+
+result = adaptive_interpolation_search(
+    arr,
+    target
+)
+
+if result != -1:
+    print(
+        "Found at index",
+        result
+    )
+else:
+    print("Not Found")
