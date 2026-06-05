ee334d4f43ecdec639476782bb92e9fe4561029d
VIPUL KUMAR PATEL <158318719+Vipul99999@users.noreply.github.com>
2026-06-05 09:29:47 +0530

    Create QuadraticBinarySearch.cpp

---
+++ Distribution-Based Searching/Quadratic Binary Search/QuadraticBinarySearch.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+int quadraticBinarySearch(
+    int arr[],
+    int n,
+    int target
+) {
+
+    int low = 0;
+    int high = n - 1;
+
+    while (
+        low <= high
+    ) {
+
+        int range =
+            high - low;
+
+        int pos =
+            low +
+            (
+                range *
+                range
+            )
+            /
+            (
+                2 *
+                (
+                    range + 1
+                )
+            );
+
+        if (
+            pos > high
+        )
+            pos =
+                low +
+                range / 2;
+
+        if (
+            arr[pos]
+            ==
+            target
+        )
+            return pos;
+
+        if (
+            arr[pos]
+            <
+            target
+        )
+            low =
+                pos + 1;
+        else
+            high =
+                pos - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+
+    int arr[] =
+    {
+        10,
+        20,
+        30,
+        40,
+        50,
+        60,
+        70,
+        80,
+        90
+    };
+
+    int n =
+        sizeof(arr)
+        /
+        sizeof(arr[0]);
+
+    cout
+    <<
+    quadraticBinarySearch(
+        arr,
+        n,
+        70
+    );
+
+    return 0;
+}
