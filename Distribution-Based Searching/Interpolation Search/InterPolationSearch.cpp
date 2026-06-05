3f2f8eaf77924172c0f4ad777776b6f33d0fe2da
VIPUL KUMAR PATEL <158318719+Vipul99999@users.noreply.github.com>
2026-06-05 09:25:26 +0530

    Create InterPolationSearch.cpp

---
+++ Distribution-Based Searching/Interpolation Search/InterPolationSearch.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+int interpolationSearch(
+    int arr[],
+    int n,
+    int target
+) {
+
+    int low = 0;
+    int high = n - 1;
+
+    while (
+        low <= high &&
+        target >= arr[low] &&
+        target <= arr[high]
+    ) {
+
+        if (
+            arr[low]
+            ==
+            arr[high]
+        )
+            return
+                arr[low]
+                ==
+                target
+                ? low
+                : -1;
+
+        int pos =
+            low +
+            (
+                (
+                    double
+                )
+                (
+                    target
+                    -
+                    arr[low]
+                )
+                *
+                (
+                    high
+                    -
+                    low
+                )
+            )
+            /
+            (
+                arr[high]
+                -
+                arr[low]
+            );
+
+        if (
+            arr[pos]
+            ==
+            target
+        )
+            return pos;
+
+        if (
+            arr[pos]
+            <
+            target
+        )
+            low =
+                pos + 1;
+        else
+            high =
+                pos - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+
+    int arr[] =
+    {
+        10,
+        20,
+        30,
+        40,
+        50,
+        60,
+        70
+    };
+
+    int n =
+        sizeof(arr)
+        /
+        sizeof(arr[0]);
+
+    cout
+    <<
+    interpolationSearch(
+        arr,
+        n,
+        50
+    );
+
+    return 0;
+}
