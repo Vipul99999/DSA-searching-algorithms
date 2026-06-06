eb721307d8d419a131dabc6df52d0d5c7b6f2c28
VIPUL KUMAR PATEL <158318719+Vipul99999@users.noreply.github.com>
2026-06-06 10:31:24 +0530

    Implement unbounded binary search algorithm

---
+++ Exponential Family Searching/Unbounded Binary Search/UnboundedBinarySearch.js
@@ -0,0 +1,80 @@
+function binarySearch(
+    arr,
+    left,
+    right,
+    target
+) {
+
+    while (left <= right) {
+
+        const mid =
+            Math.floor(
+                left +
+                (right - left) / 2
+            );
+
+        if (arr[mid] === target)
+            return mid;
+
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
+
+function unboundedBinarySearch(
+    arr,
+    target
+) {
+
+    if (arr.length === 0)
+        return -1;
+
+    if (arr[0] === target)
+        return 0;
+
+    let index = 1;
+
+    while (
+        index < arr.length &&
+        arr[index] < target
+    ) {
+        index *= 2;
+    }
+
+    const left =
+        Math.floor(index / 2);
+
+    const right =
+        Math.min(
+            index,
+            arr.length - 1
+        );
+
+    return binarySearch(
+        arr,
+        left,
+        right,
+        target
+    );
+}
+
+const arr =
+[5,10,15,20,25,30,35,40,45,50];
+
+const target = 35;
+
+const result =
+unboundedBinarySearch(
+    arr,
+    target
+);
+
+console.log(
+    result !== -1
+        ? `Found at index ${result}`
+        : "Not Found"
+);
