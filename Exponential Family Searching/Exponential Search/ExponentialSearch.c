b61264de975beb5173fdd3643fbc307c3d781c1c
VIPUL KUMAR PATEL <158318719+Vipul99999@users.noreply.github.com>
2026-06-06 10:34:48 +0530

    Create ExponentialSearch.c

---
+++ Exponential Family Searching/Exponential Search/ExponentialSearch.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
+
+int exponentialSearch(int arr[], int size, int target) {
+
+    if (size == 0)
+        return -1;
+
+    if (arr[0] == target)
+        return 0;
+
+    int bound = 1;
+
+    while (
+        bound < size &&
+        arr[bound] <= target
+    ) {
+        bound *= 2;
+    }
+
+    return binarySearch(
+        arr,
+        bound / 2,
+        (bound < size)
+            ? bound
+            : size - 1,
+        target
+    );
+}
+
+int main() {
+
+    int arr[] = {
+        5,10,15,20,
+        25,30,35,
+        40,45,50
+    };
+
+    int size =
+        sizeof(arr) /
+        sizeof(arr[0]);
+
+    int target = 35;
+
+    int result =
+        exponentialSearch(
+            arr,
+            size,
+            target
+        );
+
+    if (result != -1)
+        printf(
+            "Found at index %d\n",
+            result
+        );
+    else
+        printf(
+            "Not Found\n"
+        );
+
+    return 0;
+}
